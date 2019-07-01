b9d42e13d3ea3ea935851b2b5cccf545aba782cc
vrinceanu <daniel.vrinceanu@gmail.com>
2019-07-01 11:29:52 -0400

    commit examples

--- examples/fig2.py
+++ examples/fig2.py
@@ -7,56 +7,80 @@ import numpy as np
 T = 10
 rho = 100
 
-nrange = np.arange(50,800,50)
+with open("fig2.dat", "a") as file:
+    file.write("# T = {0:g} rho = {1:g}, L = 1\n".format(T,rho))
 
-with open("fig2.dat", "w") as file:
-    file.write("# T = {0:.2g}, rho= {1:.2g}, L = 1\n#quantum\n".format(T,rho))
+meth = "quantum"
+nrange = range(50,50,50)
+with open("fig2.dat", "a") as file:
+    file.write("\n# "+meth+"\n")
 for n in nrange:
-    k1 = rate(n,1,T,rho)
+    k2 = rate(n,1,T,rho,method=meth)
     with open("fig2.dat", "a") as file:
-        file.write("{0:4d} {1:12.4e}\n".format(n,k1)
-        )
+        file.write("{0:4d} {1:12.4e}\n".format(n,k2))
+
+meth = "semiclassical"
+nrange = range(20,800,10)
+with open("fig2.dat", "a") as file:
+    file.write("\n# "+meth+"\n")
+for n in nrange:
+    k2 = rate(n,1,T,rho,method=meth)
+    with open("fig2.dat", "a") as file:
+        file.write("{0:4d} {1:12.4e}\n".format(n,k2))
+
+meth = "P_and_S"
+nrange = range(20,800,10)
+with open("fig2.dat", "a") as file:
+    file.write("\n# "+meth+"\n")
+for n in nrange:
+    k2 = rate(n,1,T,rho,method=meth)
+    with open("fig2.dat", "a") as file:
+        file.write("{0:4d} {1:12.4e}\n".format(n,k2))
+
+meth = "PS-M"
+nrange = range(20,800,10)
+with open("fig2.dat", "a") as file:
+    file.write("\n# "+meth+"\n")
+for n in nrange:
+    k2 = rate(n,1,T,rho,method=meth)
+    with open("fig2.dat", "a") as file:
+        file.write("{0:4d} {1:12.4e}\n".format(n,k2))
 
 with open("fig2.dat", "a") as file:
-    file.write("\n#semiclassical\n")
-nrange = np.arange(20,450,10)
-for n in nrange:
-    k2 = rate(n,1,T,rho,method='semiclassical')
-    with open("fig2.dat", "a") as file:
-        file.write("{0:4d} {1:12.4e}\n".format(n,k2)
-        )
-
+    file.write("# T = {0:g} rho = {1:g}, L = n-2 \n".format(T,rho))
+    
+meth = "quantum"
+nrange = range(50,50,50)
 with open("fig2.dat", "a") as file:
-    file.write("\n# P_and_S\n")
-nrange = np.arange(20,450,10)
+    file.write("\n# "+meth+"\n")
 for n in nrange:
-    k3 = rate(n,1,T,rho,method='P_and_S')
+    k2 = rate(n,n-2,T,rho,method=meth)
     with open("fig2.dat", "a") as file:
-        file.write("{0:4d} {1:12.4e}\n".format(n,k3)
-        )
+        file.write("{0:4d} {1:12.4e}\n".format(n,k2))
 
+meth = "semiclassical"
+nrange = range(20,800,10)
 with open("fig2.dat", "a") as file:
-    file.write("\n# T = {0:.2g}, rho= {1:.2g}, L = n-2\n#quantum\n".format(T,rho))
+    file.write("\n# "+meth+"\n")
 for n in nrange:
-    k1 = rate(n,n-2,T,rho)
+    k2 = rate(n,n-2,T,rho,method=meth)
     with open("fig2.dat", "a") as file:
-        file.write("{0:4d} {1:12.4e}\n".format(n,k1)
-        )
+        file.write("{0:4d} {1:12.4e}\n".format(n,k2))
 
+meth = "P_and_S"
+nrange = range(20,800,10)
 with open("fig2.dat", "a") as file:
-    file.write("\n#semiclassical\n")
-nrange = np.arange(20,450,10)
+    file.write("\n# "+meth+"\n")
 for n in nrange:
-    k2 = rate(n,n-2,T,rho,method='semiclassical')
+    k2 = rate(n,n-2,T,rho,method=meth)
     with open("fig2.dat", "a") as file:
-        file.write("{0:4d} {1:12.4e}\n".format(n,k2)
-        )
+        file.write("{0:4d} {1:12.4e}\n".format(n,k2))
 
+meth = "PS-M"
+nrange = range(20,800,10)
 with open("fig2.dat", "a") as file:
-    file.write("\n# P_and_S\n")
-nrange = np.arange(20,450,10)
+    file.write("\n# "+meth+"\n")
 for n in nrange:
-    k3 = rate(n,n-2,T,rho,method='P_and_S')
+    k2 = rate(n,n-2,T,rho,method=meth)
     with open("fig2.dat", "a") as file:
-        file.write("{0:4d} {1:12.4e}\n".format(n,k3)
-        )
+        file.write("{0:4d} {1:12.4e}\n".format(n,k2))
